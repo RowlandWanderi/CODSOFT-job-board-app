a08c217417e2ca910fbbe3868d29b8df9f2f28a5
Rowland <rowland.wanderi@student.moringaschool.com>
2024-03-26 09:29:53 +0000

    created a change password page

--- client/src/components/ChangePassword.js
+++ client/src/components/ChangePassword.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../context/UserContext';
+import Swal from 'sweetalert2';
+
+
 
 export default function ChangePassword() {
+
+  const {changePassword} = useContext(UserContext)
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  function handleSubmit(e){
+    e.preventDefault()
+    if(newPassword !== confirmPassword){
+      Swal.fire({
+        icon:"error",
+        title:"error",
+        text:"Passwords don't match!",
+        timer: 1500
+      })
+    }else{
+      changePassword(newPassword,currentPassword)
+      setConfirmPassword("")
+      setCurrentPassword("")
+      setNewPassword("")
+    }
+  }
+
   return (
-    <div>ChangePassword</div>
+    <div className="d-flex justify-content-center mt-5" style={{ minHeight: '65vh' }}>
+      <div>
+        <h3 className="text-center">Change Password</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="currentPassword" className="form-label">Current Password:</label>
+            <input type="password" className="form-control" name="currentPassword" onChange={(e) => setCurrentPassword(e.target.value)} value={currentPassword} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="newPassword" className="form-label">New Password:</label>
+            <input type="password" className="form-control" name="newPassword" onChange={(e) => setNewPassword(e.target.value)} value={newPassword} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="confirmPassword" className="form-label">Confirm Password:</label>
+            <input type="password" className="form-control" name="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
+          </div>
+          <button type="submit" className="btn btn-primary">Change Password</button>
+        </form>
+      </div>
+    </div>
   )
 }
--- client/src/components/ViewProfile.js
+++ client/src/components/ViewProfile.js
@@ -1,7 +1,103 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../context/UserContext';
 
 export default function ViewProfile() {
+  const {currentUser, deleteAccount} = useContext(UserContext)
+  const[loading, setloading] = useState(false)
+
+  function handleDelete(){
+    deleteAccount()
+  
+  }
+
+  // Function to add a zero in front of the phone number
+  function formatPhoneNumber(phone) {
+    // Convert phone to a string
+    const phoneString = String(phone);
+    // Check if the first character is not '0', prepend '0' to the phone number
+    return phoneString.charAt(0) !== '0' ? '0' + phoneString : phoneString;
+  }
+
+
   return (
-    <div>ViewProfile</div>
+    <div className="d-flex justify-content-center mt-5" style={{ minHeight: '65vh' }}>
+      <div className="text-center">
+        <h3>My Profile</h3>
+
+        {loading ? (
+        <div class="text-center">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+        ) : currentUser ? (
+          <>
+            <div className="mb-3">
+              <button type="button" className={`btn ${currentUser.role === 'Employer' ? 'btn-primary' : 'btn-secondary'}`}>
+                {currentUser.role.charAt(0).toUpperCase() + currentUser.role.slice(1)}
+              </button>
+            </div>
+
+            <div className="row d-flex justify-content-center align-items-center">
+
+              {/*image*/}
+              <div className="col-md-4 gradient-custom text-center text-white" style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
+                <img
+                  src={currentUser && currentUser.profile_image_url || 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp'}
+                  alt="Avatar"
+                  className="img-fluid my-4"
+                  style={{ width: '90px' }}
+                />
+              </div>
+
+              {/*details*/}
+              <div className="col-md-8">
+                <div className="mb-3">
+                  <label htmlFor="fullName" className="form-label text-start">Full Name:</label>
+                  <input 
+                    type="text"
+                    className="form-control mb-3"
+                    id="firstName"
+                    value={currentUser.first_name}
+                    disabled
+                  />
+                  <input 
+                    type="text"
+                    className="form-control"
+                    id="lastName"
+                    value={currentUser.last_name}
+                    disabled
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label text-start">Email:</label>
+                  <input type="email" className="form-control" id="email" value={currentUser.email} disabled />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="phone" className="form-label text-start">Phone:</label>
+                  <input type="number" className="form-control" id="phone" value={formatPhoneNumber(currentUser.phone)} disabled />
+                </div>
+              </div>
+            </div>
+            
+
+            <div className="mb-3 text-center">
+              <label htmlFor="phone" className="form-label ">
+              <div class="alert alert-danger" role="alert">
+                CAUTION deleting the account is irreversible !
+               </div>
+              </label>
+              <input type="submit" onClick={() => handleDelete()} className="form-control bg-danger text-center text-bark fs-6 fw-semibold" id="phone" value="Delete Account"  />
+            </div>
+          </>
+        ) : (
+          <div class="alert alert-warning" role="alert">
+            Could Not get your profile
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
