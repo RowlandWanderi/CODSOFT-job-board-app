4022caf232aabc005f664a64454c5a92725be7c5
Rowland <rowland.wanderi@student.moringaschool.com>
2024-03-26 08:38:03 +0000

    added a reset password page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,4 @@
-import React,{useContext, useState} from 'react'
-import { UserContext } from '../context/UserContext'
+import React,{useState} from 'react'
 import UpdateProfile from '../components/UpdateProfile'
 import ChangePassword from '../components/ChangePassword';
 import ViewProfile from '../components/ViewProfile';
--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React,{useContext,useState} from 'react'
+import Swal from 'sweetalert2'
+
+import { UserContext } from '../context/UserContext';
 
 export default function ResetPassword() {
+
+  const {resetPassword} = useContext(UserContext)
+
+  const[first_name, setFirstName] = useState('')
+  const[last_name, setLastName] = useState('')
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  function handleSubmit(e){
+    e.preventDefault()
+    if(confirmPassword !== password){
+      Swal.fire({
+        icon: "error",
+        title: "error",
+        text: "Please enter matching passwords",
+      })
+    }else{
+      resetPassword(first_name,last_name,email,password)
+      setFirstName('')
+      setLastName('')
+      setEmail('')
+      setPassword('')
+    }
+  }
+
   return (
-    <div>ResetPassword</div>
+    <div className='container'>
+      <div className='row d-flex align-items-center justify-content-center' style={{ minHeight: '80vh' }}>
+        <div className='col-md-6'>
+          <form className='bg-secondary bg-opacity-25 p-4' style={{ borderRadius: '10px' }} onSubmit={handleSubmit}>
+            <div>
+              <h2 className='text-center'>Reset Password</h2>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="firstname" className="form-label">First name</label>
+              <input required type="text" className="form-control"  onChange={(e)=> setFirstName(e.target.value)} value={first_name} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="lastname" className="form-label">Last name</label>
+              <input required type="text" className="form-control"  onChange={(e)=> setLastName(e.target.value)} value={last_name} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email address</label>
+              <input required type="email" className="form-control"onChange={(e)=> setEmail(e.target.value)} value={email}  />
+            </div>
+           
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">New Password</label>
+              <input required type="password" className="form-control" onChange={(e)=> setPassword(e.target.value)} value={password} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="confirmPassword" className="form-label">Confirm password</label>
+              <input required type="password" className="form-control" onChange={(e)=> setConfirmPassword(e.target.value)} value={confirmPassword}  />
+            </div>
+           
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
